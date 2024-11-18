01feb984c4340fce29603f7f6ddd2b3574f8225f
Nhân_Cùi_GòZấp <phamducnhan.it@gmail.com>
2024-11-18 21:47:52 +0700

    #Deploy Web Dashboard

---
+++ library_dashboard/src/components/index.js
@@ -0,0 +1,17 @@
+import SideBar from './SideBar';
+import Content from './Content';
+import Footer from './Footer';
+import Header from './Header';
+import Breadcrumb from './Breadcrumb';
+import SidebarNav from './SidebarNav';
+import Loading from './Loading';
+
+export {
+    SideBar,
+    Content,
+    Footer,
+    Header,
+    Breadcrumb,
+    SidebarNav,
+    Loading
+}
---
+++ library_dashboard/src/pages/user/apis/index.js
@@ -0,0 +1,16 @@
+import {api} from '../../../apis/config';
+
+export const _banUser = async (userId) => {
+    const url = '/ban-user';
+    return await api.post(url, {userId});
+}
+
+export const _getUsers = async (page, limit, keyword) => {
+    const url = "/users/find-user";
+    return await api.post(url, {page, limit, keyword}); 
+}
+
+export const _getMajors = async () => {
+    const url = '/majors';
+    return await api.get(url);
+}
